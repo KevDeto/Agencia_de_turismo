773611ca18297eb24ae365e46142a782c3215825
Kevin <Kevin@192.168.0.9>
2024-12-11 18:17:29 -0300

    com1

---
+++ hackacode/src/main/java/hackacode/HackacodeApplication.java
@@ -0,0 +1,13 @@
+package hackacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackacodeApplication.class, args);
+	}
+
+}
---
+++ hackacode/src/test/java/hackacode/HackacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package hackacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackacodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
