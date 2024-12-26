a9fcb482d82a3c41fbaa96ae0a96841409a0fe7d
KevDeto <contactohuayta@gmail.com>
2024-12-26 16:44:18 -0300

    agregado los dto de paquete y servicio

---
+++ src/main/java/hackacode/model/dto/PaqueteTuristicoDto.java
@@ -0,0 +1,5 @@
+package hackacode.model.dto;
+
+public class PaqueteTuristicoDto {
+
+}
---
+++ src/main/java/hackacode/model/dto/ServicioTuristicoDto.java
@@ -0,0 +1,12 @@
+package hackacode.model.dto;
+
+import java.util.Date;
+
+public class ServicioTuristicoDto {
+	private Long UUID;
+	private String nombre;
+	private String descripcion;
+	private String destino_servicio;
+	private Date fecha_servicio;
+	private double costo_servicio;
+}
