2ed5e750c7183ca9c2bbf6e8836157c9025757b6
KevDeto <contactohuayta@gmail.com>
2024-12-28 18:14:44 -0300

    se modifico la relacion

--- src/main/java/hackacode/model/entity/ServicioTuristico.java
+++ src/main/java/hackacode/model/entity/ServicioTuristico.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,7 +32,12 @@ public class ServicioTuristico {
 	private Date fecha_servicio;
 	private double costo_servicio;
 	
-	@ManyToMany(mappedBy = "servicios")
+	@ManyToMany /*(mappedBy = "servicios")*/
+	@JoinTable(
+			name = "servicio_paquete",
+			joinColumns = @JoinColumn(name = "servicio_uuid"),
+			inverseJoinColumns = @JoinColumn(name  = "paquete_uuid")
+			)
 	private Set<PaqueteTuristico> paquetes;
 	
 }
