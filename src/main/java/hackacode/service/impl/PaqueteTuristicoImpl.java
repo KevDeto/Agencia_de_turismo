d60eb302f33e1320e83e36fd4971fb94aa90a208
KevDeto <contactohuayta@gmail.com>
2024-12-28 18:17:34 -0300

    agregado funcionamiento mediante ayuda de utils

--- src/main/java/hackacode/service/impl/PaqueteTuristicoImpl.java
+++ src/main/java/hackacode/service/impl/PaqueteTuristicoImpl.java
@@ -2,18 +2,17 @@ package hackacode.service.impl;
 
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import hackacode.model.dao.IPaqueteTuristicoDao;
-import hackacode.model.dao.IServicioTuristicoDao;
 import hackacode.model.dto.PaqueteTuristicoDto;
 import hackacode.model.entity.PaqueteTuristico;
 import hackacode.model.entity.ServicioTuristico;
 import hackacode.service.IPaqueteTuristicoService;
+import hackacode.utils.RelacionadorServicios;
 
 @Service
 public class PaqueteTuristicoImpl implements IPaqueteTuristicoService{
@@ -21,21 +20,21 @@ public class PaqueteTuristicoImpl implements IPaqueteTuristicoService{
     @Autowired
     private IPaqueteTuristicoDao paqueteTuristicoDao;
     @Autowired
-    private IServicioTuristicoDao servicioTuristicoDao;
+    private RelacionadorServicios relacionadorServicios;
 
     @Transactional
     @Override
     public PaqueteTuristico save(PaqueteTuristicoDto paqueteTuristicoDto) {
-        Set<ServicioTuristico> servicios = paqueteTuristicoDto.getServicio_turistico().stream()
-        		.map(UUID -> servicioTuristicoDao.findById(Long.valueOf(UUID))
-        				.orElseThrow(() -> new RuntimeException("Servicio no encontrado: " + UUID)))
-                .collect(Collectors.toSet());
     	
-    	PaqueteTuristico paqueteTuristico = PaqueteTuristico.builder()
-                .UUID(paqueteTuristicoDto.getUUID())
-                .costo_paquete(paqueteTuristicoDto.getCosto_paquete())
-                .servicios(servicios)
-                .build();
+    	Set<ServicioTuristico> servicios = relacionadorServicios
+    			.obtenerServiciosRelacionados(paqueteTuristicoDto.getServicio_turistico());
+
+        PaqueteTuristico paqueteTuristico = PaqueteTuristico.builder()
+            .UUID(paqueteTuristicoDto.getUUID())
+            .costo_paquete(paqueteTuristicoDto.getCosto_paquete())
+            .servicios(servicios) // Relación opcional
+            .build();
+
         return paqueteTuristicoDao.save(paqueteTuristico);
     }
     
--- src/main/java/hackacode/service/impl/ServicioTuristicoImpl.java
+++ src/main/java/hackacode/service/impl/ServicioTuristicoImpl.java
@@ -2,18 +2,17 @@ package hackacode.service.impl;
 
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import hackacode.model.dao.IPaqueteTuristicoDao;
 import hackacode.model.dao.IServicioTuristicoDao;
 import hackacode.model.dto.ServicioTuristicoDto;
 import hackacode.model.entity.PaqueteTuristico;
 import hackacode.model.entity.ServicioTuristico;
 import hackacode.service.IServicioTuristicoService;
+import hackacode.utils.RelacionadorPaquetes;
 
 @Service
 public class ServicioTuristicoImpl implements IServicioTuristicoService{
@@ -21,26 +20,26 @@ public class ServicioTuristicoImpl implements IServicioTuristicoService{
     @Autowired
     private IServicioTuristicoDao servicioTuristicoDao;
     @Autowired
-    private IPaqueteTuristicoDao paqueteTuristicoDao;
+    private RelacionadorPaquetes relacionadorPaquetes;
 
     @Transactional
     @Override
     public ServicioTuristico save(ServicioTuristicoDto servicioTuristicoDto) {
-        Set<PaqueteTuristico> paquetes = servicioTuristicoDto.getPaquete_turistico().stream()
-        		.map(UUID -> paqueteTuristicoDao.findById(Long.valueOf(UUID))
-         				.orElseThrow(() -> new RuntimeException("Paquete no encontrado: " + UUID)))
-                .collect(Collectors.toSet());
     	
-    	ServicioTuristico paqueteTuristico = ServicioTuristico.builder()
-                .UUID(servicioTuristicoDto.getUUID())
-                .nombre(servicioTuristicoDto.getNombre())
-                .descripcion(servicioTuristicoDto.getDescripcion())
-                .destino_servicio(servicioTuristicoDto.getDestino_servicio())
-                .fecha_servicio(servicioTuristicoDto.getFecha_servicio())
-                .costo_servicio(servicioTuristicoDto.getCosto_servicio())
-                .paquetes(paquetes)
-                .build();
-        return servicioTuristicoDao.save(paqueteTuristico);
+    	 Set<PaqueteTuristico> paquetes = relacionadorPaquetes
+    			 .obtenerPaquetesRelacionados(servicioTuristicoDto.getPaquete_turistico());
+
+        ServicioTuristico servicioTuristico = ServicioTuristico.builder()
+            .UUID(servicioTuristicoDto.getUUID())
+            .nombre(servicioTuristicoDto.getNombre())
+            .descripcion(servicioTuristicoDto.getDescripcion())
+            .destino_servicio(servicioTuristicoDto.getDestino_servicio())
+            .fecha_servicio(servicioTuristicoDto.getFecha_servicio())
+            .costo_servicio(servicioTuristicoDto.getCosto_servicio())
+            .paquetes(paquetes) // Relación opcional
+            .build();
+
+        return servicioTuristicoDao.save(servicioTuristico);
     }
 
     @Transactional(readOnly = true)
