a9518a273f3e8ffa8d24e27d2d64ad0af533d403
KevDeto <contactohuayta@gmail.com>
2025-01-04 11:32:08 -0300

    com1

--- src/main/java/hackacode/model/dto/PaqueteTuristicoDTO.java
+++ src/main/java/hackacode/model/dto/PaqueteTuristicoDTO.java
@@ -1,18 +1,5 @@
 package hackacode.model.dto;
 
-import java.util.List;
-import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.Builder;
-import lombok.Data;
-import lombok.ToString;
-
-@Data
-@Builder
 public class PaqueteTuristicoDTO {
-    private Long codigoPaquete;
-    private List<ServicioTuristicoDTO> listaServiciosIncluidos;
-    private Double costoPaquete;
+
 }
