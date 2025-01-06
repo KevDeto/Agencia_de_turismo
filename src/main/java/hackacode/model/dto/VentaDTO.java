f82679944007fa665ee5e91049b36e1cab30cefa
KevDeto <contactohuayta@gmail.com>
2025-01-06 04:12:38 -0300

    se agrego servicio y paquete

--- src/main/java/hackacode/model/dto/VentaDTO.java
+++ src/main/java/hackacode/model/dto/VentaDTO.java
@@ -1,9 +1,12 @@
 package hackacode.model.dto;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import hackacode.model.entity.PaqueteTuristico;
+import hackacode.model.entity.ServicioTuristico;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,8 +16,10 @@ public class VentaDTO {
 	
 	@JsonIgnore
 	private Long UUID;	
-	private Date fecha_venta;
+	private LocalDate fecha_venta;
 	private double monto_total;
 	private Long cliente_uuid;
 	private Long empleado_uuid;
+	private Long servicio_uuid;
+	private Long paquete_uuid;
 }
