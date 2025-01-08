65f0dcfd419019728eab22178e4e6cba087c8fa7
KevDeto <contactohuayta@gmail.com>
2025-01-08 03:44:58 -0300

    renombre clases y metodos

--- src/main/java/hackacode/service/IEmpleadoService.java
+++ src/main/java/hackacode/service/IEmpleadoService.java
@@ -6,9 +6,14 @@ import hackacode.model.dto.EmpleadoDTO;
 import hackacode.model.entity.Empleado;
 
 public interface IEmpleadoService {
-    List<Empleado> listAll();
-    Empleado save(EmpleadoDTO empleado);
-    Empleado findById(Long id);
-    void delete(Empleado empleado);
-    boolean existsById(Long id);
+	
+	List<Empleado> listarEmpleados();
+
+	Empleado crearEmpleado(EmpleadoDTO empleado);
+
+	Empleado obtenerEmpleadoPorId(Long id);
+
+	void eliminarEmpleado(Empleado empleado);
+
+	boolean existeEmpleadoPorId(Long id);
 }
