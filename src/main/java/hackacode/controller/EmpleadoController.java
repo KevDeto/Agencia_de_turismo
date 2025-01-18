fd8f5e288b59485c8293b216106380ae354f86a0
KevDeto <contactohuayta@gmail.com>
2025-01-18 16:07:51 -0300

    cambie la logica

--- src/main/java/hackacode/controller/EmpleadoController.java
+++ src/main/java/hackacode/controller/EmpleadoController.java
@@ -28,128 +28,98 @@ public class EmpleadoController {
     private IEmpleadoService empleadoService;
 
     @GetMapping("empleados")
-    public ResponseEntity<?> showAll() {
-        List<Empleado> getList = empleadoService.listarEmpleados();
-        if (getList == null) {
-            return new ResponseEntity<>(MensajeResponse.builder()
-                    .mensaje("No hay registros")
-                    .objeto(null)
-                    .build(),
-                    HttpStatus.OK);
+    public ResponseEntity<MensajeResponse> showAll() {
+        List<Empleado> listaEmpleados = empleadoService.listarEmpleados();
+        if(listaEmpleados.isEmpty()) {
+        	return ResponseEntity.ok(MensajeResponse.builder()
+        			.mensaje("No se han encontrado registros.")
+        			.objeto(listaEmpleados)
+        			.build());
         }
-        return new ResponseEntity<>(MensajeResponse.builder()
-                .mensaje("")
-                .objeto(getList)
-                .build(),
-                HttpStatus.OK);
+        return ResponseEntity.ok(MensajeResponse.builder()
+                .mensaje("Empleados recuperados correctamente.")
+                .objeto(listaEmpleados)
+                .build());
     }
 
     @PostMapping("empleado")
-    public ResponseEntity<?> create(@RequestBody EmpleadoDTO empleadoDTO) {
-        Empleado empleadoSave = null;
+    public ResponseEntity<MensajeResponse> create(@RequestBody EmpleadoDTO empleadoDTO) {
         try {
-        	empleadoSave = empleadoService.crearEmpleado(empleadoDTO);
-            return new ResponseEntity<>(MensajeResponse
-                    .builder()
-                    .mensaje("Guardado correctamente")
-                    .objeto(empleadoDTO)
-                    .build(),
-                    HttpStatus.CREATED);
-        } catch (DataAccessException exDt) {
-            return new ResponseEntity<>(MensajeResponse.builder()
-                    .mensaje(exDt.getMessage())
-                    .objeto(null)
-                    .build(),
-                    HttpStatus.METHOD_NOT_ALLOWED);
+        	Empleado empleadoSave = empleadoService.crearEmpleado(empleadoDTO);
+        	return ResponseEntity.status(HttpStatus.CREATED)
+        			.body(MensajeResponse.builder()
+        					.mensaje("Empleado creado correctamente.")
+        					.objeto(empleadoSave)
+        					.build());
+        } catch (DataAccessException e) {
+        	return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+        			.body(MensajeResponse.builder()
+        					.mensaje("Error al crear empleado: " + e.getMessage())
+        					.objeto(null)
+        					.build());
         }
     }
 
     @PutMapping("empleado/{id}")
-    public ResponseEntity<?> update(@RequestBody EmpleadoDTO empleadoDTO, @PathVariable Long id) {
-        Empleado empleadoUpdate = null;
-        try {
-        	Empleado findCliente = empleadoService.obtenerEmpleadoPorId(id);
-            if (empleadoService.existeEmpleadoPorId(id)) {
-            	empleadoDTO.setUUID(id);
-            	empleadoUpdate = empleadoService.crearEmpleado(empleadoDTO);
-
-                return new ResponseEntity<>(MensajeResponse
-                        .builder()
-                        .mensaje("Guardado correctamente")
-                        .objeto(EmpleadoDTO.builder()
-                                .UUID(empleadoUpdate.getUUID())
-                                .nombre(empleadoUpdate.getNombre())
-                                .apellido(empleadoUpdate.getApellido())
-                                .dni(empleadoUpdate.getDni())
-                                .nacionalidad(empleadoUpdate.getNacionalidad())
-                                .email(empleadoUpdate.getEmail())
-                                .celular(empleadoUpdate.getCelular())
-                                .fecha_nac(empleadoUpdate.getFecha_nac())
-                                .direccion(empleadoUpdate.getDireccion())
-                                .cargo(empleadoUpdate.getCargo())
-                                .sueldo(empleadoUpdate.getSueldo())
-                                .build())
-                        .build(),
-                        HttpStatus.CREATED);
-            } else {
-                return new ResponseEntity<>(MensajeResponse.builder()
-                        .mensaje("El registro que intenta actualizar no se encuentra en la base de datos")
-                        .objeto(null)
-                        .build(),
-                        HttpStatus.NOT_FOUND);
-            }
-
-        } catch (DataAccessException exDt) {
-            return new ResponseEntity<>(MensajeResponse.builder()
-                    .mensaje(exDt.getMessage())
-                    .objeto(null)
-                    .build(),
-                    HttpStatus.METHOD_NOT_ALLOWED);
-        }
+    public ResponseEntity<MensajeResponse> update(@RequestBody EmpleadoDTO empleadoDTO, @PathVariable Long id) {
+    	if(!empleadoService.existeEmpleadoPorId(id)) {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+    				.body(MensajeResponse.builder()
+    						.mensaje("Empleado con ID " + id + " no encontrado.")
+    						.objeto(null)
+    						.build());
+    	}
+    	try {
+			empleadoDTO.setUUID(id);
+			Empleado empleadoUpdate = empleadoService.crearEmpleado(empleadoDTO);
+			return ResponseEntity.ok(MensajeResponse.builder()
+							.mensaje("Empleado actualizado correctamente.")
+							.objeto(empleadoUpdate)
+							.build());
+		} catch (DataAccessException e) {
+			return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+					.body(MensajeResponse.builder()
+							.mensaje("Error al modificar empleado: " + e.getMessage())
+							.objeto(null)
+							.build());
+		}
     }
 
     @DeleteMapping("empleado/{id}")
-    //@ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<?> delete(@PathVariable Long id) {
-        try {
+    public ResponseEntity<MensajeResponse> delete(@PathVariable Long id) {
+    	if(!empleadoService.existeEmpleadoPorId(id)) {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+    				.body(MensajeResponse.builder()
+    						.mensaje("Empleado con ID " + id +" no encontrado.")
+    						.objeto(null)
+    						.build());
+    	}
+    	try {
             Empleado empleadoDelete = empleadoService.obtenerEmpleadoPorId(id);
             empleadoService.eliminarEmpleado(empleadoDelete);
-            return new ResponseEntity<>(empleadoDelete, HttpStatus.NO_CONTENT);
-        } catch (DataAccessException exDt) {
-            return new ResponseEntity<>(MensajeResponse.builder()
-                    .mensaje(exDt.getMessage())
-                    .objeto(null)
-                    .build(),
-                    HttpStatus.METHOD_NOT_ALLOWED);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (DataAccessException e) {
+        	return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+        			.body(MensajeResponse.builder()
+        					.mensaje("Error al eliminar empleado: " + e.getMessage())
+        					.objeto(null)
+        					.build());
         }
     }
 
     @GetMapping("empleado/{id}")
     public ResponseEntity<?> showById(@PathVariable Long id) {
         Empleado empleado = empleadoService.obtenerEmpleadoPorId(id);
-        if (empleado == null) {
-            return new ResponseEntity<>(MensajeResponse.builder()
-                    .mensaje("El registro que intenta buscar no existe")
-                    .objeto(null)
-                    .build(),
-                    HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(MensajeResponse.builder()
-                .mensaje("")
-                .objeto(EmpleadoDTO.builder()
-                        .UUID(empleado.getUUID())
-                        .nombre(empleado.getNombre())
-                        .apellido(empleado.getApellido())
-                        .dni(empleado.getDni())
-                        .nacionalidad(empleado.getNacionalidad())
-                        .email(empleado.getEmail())
-                        .celular(empleado.getCelular())
-                        .fecha_nac(empleado.getFecha_nac())
-                        .direccion(empleado.getDireccion())
-                        .cargo(empleado.getCargo())
-                        .sueldo(empleado.getSueldo())
-                        .build())
-                .build(),
-                HttpStatus.OK);
+    	if(empleado == null) {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+    				.body(MensajeResponse.builder()
+    						.mensaje("Empleado con ID " + id + " no encontrado.")
+    						.objeto(null)
+    						.build());
+    	}
+        return ResponseEntity.ok(MensajeResponse.builder()
+        		.mensaje("Empleado recuperado correctamente.")
+        		.objeto(empleado)
+        		.build());
     }
 }
