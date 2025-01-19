4bca41b2b5c1055aaa9ac20151b0f432d39d0dbd
KevDeto <contactohuayta@gmail.com>
2025-01-19 18:56:14 -0300

    update

--- src/main/java/hackacode/model/repository/IClienteRepository.java
+++ src/main/java/hackacode/model/repository/IClienteRepository.java
@@ -2,6 +2,7 @@ package hackacode.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import hackacode.model.dto.ClienteDTO;
 import hackacode.model.entity.Cliente;
 
 public interface IClienteRepository extends JpaRepository<Cliente, Long>{
--- src/main/java/hackacode/model/repository/IEmpleadoRepository.java
+++ src/main/java/hackacode/model/repository/IEmpleadoRepository.java
@@ -2,6 +2,7 @@ package hackacode.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import hackacode.model.dto.EmpleadoDTO;
 import hackacode.model.entity.Empleado;
 
 public interface IEmpleadoRepository extends JpaRepository<Empleado, Long>{
